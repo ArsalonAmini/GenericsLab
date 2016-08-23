a4f49dae51a10e17c9d22f32a4faafe99c2e278e
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-23 12:31:16 -0500

    pre lunch commit of generics lab, IEnumerator GetEnumerator() method completed, not tested

---
+++ GenericsLab/GenericsLab/GenericList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsLab
+{
+    public class GenericList<T>
+
+    {
+
+        private T[] innerArray;
+
+        //innerArray = new T[0] //creating an internal array
+
+        public void AddToList()
+        {
+            int[] internalArray = new int[10];
+            int[] internalArrayTwo = new int[11];
+            internalArray[] = internalArrayTwo[];
+
+        public void AddToList() //Method
+        {
+            T[] = tempInnerArray = new T[innerArray.Length + 1];
+            for (int i = 0; i < innerArray.Length, i++)
+            {
+                tempInnerArray[i] = innerArray[i];
+            }
+            tempInnerArray[innerArray.Length] = thing;
+            innerArray = tempInnerArray;
+        }
+        public void RemoveFromList()
+        {
+            int[] subtractArray = new int[9];
+            int[] internalArrayThree = int internalArrayTwo[];
+        }
+        public void PlusOperator()
+        {
+
+        }
+        public void SubtractOperator()
+    }
+}
---
+++ GenericsLab/GenericsLab/IterableClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsLab
+{
+    class IterableClass
+    {
+        List<T> name;
+        List<T> variable;
+        int iterationCount;
+
+        public IterableClass()
+        {
+            name = new List<string>() { "value", "Value", "Value" };
+            variable = new List<int>() { 1, 2, 2 };
+            iterationCount = 0;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < name.Count; i++)
+            {
+                yield return 5;
+                yield return name[i];
+                yield return "";
+                yield return variable[i];
+                yield return "/n";
+            }
+            yield return "That's it boss";
+        }
+
+    }
+}
